b9af765fccdeabce8a88e667cd181691c1a013c2
Yusuf Hilmi <177yusuf177@gmail.com>
2019-07-01 18:16:20 +0300

    Working Model Without Links

--- Play/simple_nodes.py
+++ Play/simple_nodes.py
@@ -4,7 +4,6 @@ import os
 from flask import Flask, request, session, g, redirect, url_for, \
     abort, render_template, flash, json, jsonify
 
-
 """     --------  For Local Access   -------------
 SITE_ROOT = os.path.realpath(os.path.dirname(__file__))
 json_url = os.path.join(SITE_ROOT, "static", "IBM_composite_query.json")
@@ -12,57 +11,59 @@ data = json.load(open(json_url, encoding="mbcs"))      #encoding fixed mbcs is t
 print(str(data))
 """
 
-
 app = Flask(__name__)
-
 
 
 @app.route('/favicon.ico')
 def favicon():
     return "malak"
 
+
 @app.route('/trial', methods=['GET', 'POST'])
 def trial():
-
-
     return render_template('cloud_2.html')
-
-
 
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
     nodes = []
     links = []
+    words = []
+    articles = []
 
     SITE_ROOT = os.path.realpath(os.path.dirname(__file__))
     json_url = os.path.join(SITE_ROOT, "static", "IBM_composite_query.json")
     data = json.load(open(json_url, encoding="mbcs"))  # encoding fixed mbcs is the correct one
     index_num = len(data['results'][1]['enriched_text']['entities'])
 
-
-
     # index_res = len(data['results'])
 
     #  index_num = len(request.json['results'][1]['enriched_text']['entities'])
 
     count = 0
-    nodes.insert(0, {'id': 0, 'label': 'NODES', 'level': 1})
+    label_count = 0
 
+    for j in range(len(data['results'])):
+        articles.append(data['results'][j]['title'])
+        # print(articles[j])
+        nodes.append({'id': j, 'label': articles[j], 'level': 1})
+        count += 1
 
-    for i in range(index_num):
-        if count > 9:
-            break
-        else:
+    for j in range(len(articles)):
+        label_count += len(data['results'][j]['enriched_text']['entities'])
+        print(label_count)
             # word = request.json['results'][1]['enriched_text']['entities'][i]['text']
-            word = data['results'][4]['enriched_text']['entities'][i]
-
-            # if word['type'] == 'Person':
-            # print(word)
-            nodes.append({'id': count+1, 'label': word['text'], 'level': 1})
-            links.append({'source': count + 1, 'target': 0})
-            count += 1
-
+        for i in range(len(data['results'][j]['enriched_text']['entities'])):
+            # print(words[i])
+            if data['results'][j]['enriched_text']['entities'][i]['type'] == 'Location':
+                words.append(data['results'][j]['enriched_text']['entities'][i]['text'])
+                nodes.append({'id': count, 'label': words[-1], 'level': 2})
+                print(nodes[-1])
+                links.append({'source': count, 'target': j})
+                count += 1
+        print("-----------------------------------------")
+    # print(nodes)
+    # print(links)
     return render_template('cloud.html', nodes=json.dumps(nodes, indent=2), links=json.dumps(links, indent=2))
 
 
@@ -70,4 +71,3 @@ port = os.getenv('VCAP_APP_PORT', '8000')
 
 if __name__ == '__main__':
     app.run(port=int(port), debug=True)
-
